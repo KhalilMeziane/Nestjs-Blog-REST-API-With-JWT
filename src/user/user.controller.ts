445c03d2d96d2e71663596c50d4140ad35aefe61
MezianeKhalil <khalilmez2000@gmail.com>
2022-02-25 18:47:35 +0100

    buil edit profile endpoint

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,22 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, UseGuards, Patch, Body } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { User } from '@prisma/client';
 import { GetUser } from 'src/auth/decorator/get-user.decorator';
+import { UserService } from './user.service';
 
 @UseGuards(AuthGuard('jwt'))
 @Controller('user')
 export class UserController {
 
+    constructor(private userService: UserService){}
+
     @Get()
     getProfile(@GetUser() user: User){
         return user
+    }
+
+    @Patch()
+    editProfile(@GetUser() user: User, @Body() body){
+        return this.userService.editProfile(user.id,body)
     }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,10 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ForbiddenException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UserService {
 
+    constructor(private prisma : PrismaService){}
     
-    async getProfile(){
 
+    async editProfile(id,body){
+        const user = await this.prisma.user.findUnique({
+            where:{
+                id
+            }
+        })
+        if(!user) throw new ForbiddenException('credentials not correct')
+        const updatedUser = await this.prisma.user.update({
+            where:{
+                id
+            },
+            data:{
+                ...body
+            }
+        })
+        return updatedUser
     }
 }
